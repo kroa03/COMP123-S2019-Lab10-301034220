097b2273144f93d4cb5741c3ae8f5265ca575052
Koolait Roa <kroa1@my.centennialcollege.ca>
2019-07-18 16:01:16 -0400

    pre-created all of the Forms in the Program.cs file

--- COMP123-S2019-Lab10-301034220/Program.cs
+++ COMP123-S2019-Lab10-301034220/Program.cs
@@ -8,6 +8,7 @@ namespace COMP123_S2019_Lab10_301034220
 {
     static class Program
     {
+        public static Dictionary<FormName, Form> Forms;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +17,14 @@ namespace COMP123_S2019_Lab10_301034220
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StartForm());
+            //Application.Run(new StartForm());
+
+            Forms = new Dictionary<FormName, Form>();
+            Forms.Add(FormName.START_FORM, new StartForm());
+            Forms.Add(FormName.MAIN_FORM, new MainForm());
+            Forms.Add(FormName.END_FORM, new EndForm());
+
+            Application.Run(Forms[FormName.START_FORM]);
         }
     }
 }
