bdb6a72a0a0010acd407026c8bf003cd9378bf7b
Koolait Roa <kroa1@my.centennialcollege.ca>
2019-07-18 15:57:35 -0400

    made a form closing event for main form and end form. and nextbutton click event to redirect from main form to end form

---
+++ COMP123-S2019-Lab10-301034220/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lab10_301034220
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            EndForm endForm = new EndForm();
+            endForm.Show();
+            this.Hide();
+        }
+    }
+}
